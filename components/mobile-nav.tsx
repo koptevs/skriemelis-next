3f0afbee6c6f6b4bc9bb205501c85f51dac0e53c
Igors Koptevs <igors.koptevs@gmail.com>
2024-08-14 15:39:11 +0300

    Mobile nav in progress

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import {
     Sheet,
     SheetContent,
@@ -11,8 +12,34 @@ import {
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    SignOutButton,
+    UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
+
+const NavContent = () => {
+    return (
+        <div className="mt-8 flex flex-col items-center justify-between gap-8">
+            <SheetClose asChild>
+                <Link className="" href="/">
+                    Home
+                </Link>
+            </SheetClose>
+            <SheetClose asChild>
+                <Link className="" href="/landing">
+                    Landing
+                </Link>
+            </SheetClose>
+            <SheetClose asChild>
+                <ModeToggle />
+            </SheetClose>
+        </div>
+    );
+};
 
 const MobileNav = () => {
     return (
@@ -24,36 +51,58 @@ const MobileNav = () => {
                     </Button>
                 </SheetTrigger>
                 <SheetContent side="right" className="w-[300px]">
-                    <SheetHeader>
-                        <SheetTitle>Switch Mode</SheetTitle>
-
-                        <SheetDescription>
-                            <ModeToggle />
-                        </SheetDescription>
-                    </SheetHeader>
-                    <div className="mt-8 flex flex-col items-center justify-between gap-8">
-                        <SheetClose asChild>
-                            <Link className="" href="/">
-                                Home
-                            </Link>
-                        </SheetClose>
-                        <SheetClose asChild>
-                            <Link className="" href="/landing">
-                                Landing
-                            </Link>
-                        </SheetClose>
-
+                    <SheetClose asChild>
+                        <Link
+                            href="/"
+                            className="flex items-center justify-center gap-2"
+                        >
+                            <Image
+                                src="/images/site-logo.png"
+                                width={23}
+                                height={23}
+                                alt="Site Logo"
+                            />
+                            <div className="py-2 font-bold">
+                                SK-
+                                <span className="text-orange-600">NEXT</span>
+                            </div>
+                        </Link>
+                    </SheetClose>
+                    <SheetClose asChild>
+                        <NavContent />
+                    </SheetClose>
+                    <div className="mt-3 flex flex-col gap-3">
                         <SignedOut>
-                            <SignInButton />
+                            <SheetClose asChild>
+                                <Button
+                                    asChild
+                                    // className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent"
+                                >
+                                    <Link href="/sign-in">
+                                        <span>Sign In</span>
+                                    </Link>
+                                </Button>
+                                {/* <SignInButton /> */}
+                            </SheetClose>
+                            <SheetClose asChild>
+                                <Button
+                                    asChild
+                                    // className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent"
+                                >
+                                    <Link href="/sign-up">
+                                        <span>Sign Up</span>
+                                    </Link>
+                                </Button>
+                                {/* <SignInButton /> */}
+                            </SheetClose>
                         </SignedOut>
                         <SignedIn>
-                            <UserButton
-                                appearance={{
-                                    elements: {
-                                        avatarBox: "h-7 w-7",
-                                    },
-                                }}
-                            />
+                            {/* <Link href="/sign-out">Sign out</Link> */}
+                            <SignOutButton>
+                                <SheetClose asChild>
+                                    <Button>Sign Out</Button>
+                                </SheetClose>
+                            </SignOutButton>
                         </SignedIn>
                     </div>
                 </SheetContent>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,10 +8,10 @@ import MobileNav from "./mobile-nav";
 
 const Navbar = () => {
     return (
-        <nav className="shadow-iky/10 fixed z-50 flex w-full items-center justify-between bg-background p-2 shadow-md dark:shadow-none">
+        <nav className="fixed z-50 flex w-full items-center justify-between bg-background p-2 shadow-md shadow-iky/10 dark:shadow-none">
             <Link href="/" className="flex items-center justify-center gap-1">
                 <Image
-                    src="/images/zvans.png"
+                    src="/images/site-logo.png"
                     alt="Site Logo"
                     width={20}
                     height={20}
@@ -24,28 +24,30 @@ const Navbar = () => {
             <div className="flex self-center font-bold tracking-wider sm:text-2xl">
                 GLOBAL SEARCH
             </div>
-            <div className="flex items-center justify-between gap-2 md:gap-4">
-                <Link className="px-1 py-2 max-sm:hidden" href="/">
-                    Home
-                </Link>
-                <Link className="px-1 py-2 pr-2 max-sm:hidden" href="/landing">
-                    Landing
-                </Link>
-                <ModeToggle />
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton
-                        appearance={{
-                            elements: {
-                                avatarBox: "h-7 w-7",
-                            },
-                        }}
-                    />
-                </SignedIn>
-                <MobileNav />
+            <div className="max-sm:hidden">
+                <div className="flex items-center justify-between gap-2 md:gap-4">
+                    <Link className="px-1 py-2" href="/">
+                        Home
+                    </Link>
+                    <Link className="px-1 py-2 pr-2" href="/landing">
+                        Landing
+                    </Link>
+                    <ModeToggle />
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                                elements: {
+                                    avatarBox: "h-7 w-7",
+                                },
+                            }}
+                        />
+                    </SignedIn>
+                </div>
             </div>
+            <MobileNav />
         </nav>
     );
 };
